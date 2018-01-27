7d5631dbcdef33f16904d8992866c195e0b71ba5
Nikita Nafranets <eddimensi@gmail.com>
2018-01-27 19:36:58 +0300

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import './index.css'
+import './index.css';
 import * as YandexMetrika from './modules/YandexMetrika';
 import * as GoogleAnalytics from './modules/GoogleAnalytics';
 import * as Sentry from './modules/Sentry';
 
 window.addEventListener('load', () => {
-  let width  = window.screen.width * window.devicePixelRatio;
-  let height = window.screen.height * window.devicePixelRatio;
+  const width = window.screen.width * window.devicePixelRatio;
+  const height = window.screen.height * window.devicePixelRatio;
 
   document.getElementById('resolution').innerHTML = `${width}&times;${height}`;
 
